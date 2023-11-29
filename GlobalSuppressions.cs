d124dcf3f23c06fae770525a3537c28ac309514c
Ashley Cave <45659989+AHollowedHunter@users.noreply.github.com>
2023-11-29 22:58:23 +0000

    Create GlobalSuppressions.cs

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// Copyright (c) Ashley Cave
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Diagnostics.CodeAnalysis;
+
+// Steam Project
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1636:FileHeaderCopyrightTextMustMatch", Justification = "Not MS code")]
